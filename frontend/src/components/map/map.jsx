158d7695ac8a38f2afe6bd2d7f354be53ba6f664
aldenbaughman <aldenb@bu.edu>
2025-04-21 18:20:47 -0400

    Sidebar with changing lat & lng added

--- frontend/src/components/map/map.jsx
+++ frontend/src/components/map/map.jsx
@@ -6,7 +6,9 @@ import { MapContainer, TileLayer, useMapEvents, Marker, Popup} from 'react-leafl
 
 
 function Map() {
-    const [position, setPosition] = useState([42.350876, -71.106918])
+    var [position, setPosition] = useState([42.350876, -71.106918])
+    var [latitude, setLatitude] = useState(42.350876)
+    var [longitude, setLongitude] = useState(-71.106918)
 
     function LocateUser(){
         var popup = L.popup()
@@ -17,34 +19,32 @@ function Map() {
             locationfound(e){
                 setPosition(e.latlng)   
                 map.flyTo(e.latlng, 13)
-                popup
-                .setLatLng(e.latlng)
-                .setContent("Youre Position is: " + e.latlng.toString())
-                .openOn(map)
+                setLatitude(e.latlng.lat)
+                setLongitude(e.latlng.lng)
             }
 
         })
         return (
             <Marker position={position}>
-                <Popup>Your location is {position.toString}</Popup>
+                <Popup>YOOOOO</Popup>
             </Marker>
         )
     }
     
-    function MyComponent() {
+    function MapClick() {
         var popup = L.popup()
         const map = useMapEvents({
             click(e){
-                setPosition(e.latlng)
+                setPosition(e.latlng)   
                 map.flyTo(e.latlng, 13)
-                
+                setLatitude(e.latlng.lat)
+                setLongitude(e.latlng.lng)
             }
         })
         //CRASHES WHEN THIS MARKER IS CLICKED
         return (
             <Marker position={position}>
-
-                <Popup>You Clicked at {position.toString}</Popup>
+                <Popup>Dont touch me</Popup>
             </Marker>
         )
       }
@@ -54,10 +54,10 @@ function Map() {
             <div id="sidebar">
                 <ul>
                     <li>
-                        <h1>LAT: </h1>
+                        <h1>LAT: { latitude }</h1>
                     </li>
                     <li>
-                        <h1>LONG: </h1>
+                        <h1>LONG: { longitude }</h1>
                     </li>
                     <li>
                         <h1>CITY: </h1>
@@ -73,7 +73,7 @@ function Map() {
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
                     <LocateUser/>
-                    <MyComponent/>
+                    <MapClick/>
                 </MapContainer>
             </div>
         </div>
