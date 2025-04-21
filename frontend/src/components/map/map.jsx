9f1d7605d965f3d2bc6200474559668bf38538cb
aldenbaughman <aldenb@bu.edu>
2025-04-21 14:31:23 -0400

    Simple Sidebar + Map added

--- frontend/src/components/map/map.jsx
+++ frontend/src/components/map/map.jsx
@@ -2,34 +2,81 @@ import './map.css'
 import 'leaflet/dist/leaflet.css'
 
 import { useState } from 'react'
-import { MapContainer, Popup, TileLayer, useMap, useMapEvents } from 'react-leaflet'
+import { MapContainer, TileLayer, useMapEvents, Marker, Popup} from 'react-leaflet'
 
 
 function Map() {
+    const [position, setPosition] = useState([42.350876, -71.106918])
+
+    function LocateUser(){
+        var popup = L.popup()
+        const map = useMapEvents({
+            preclick(e){
+                map.locate()
+            },
+            locationfound(e){
+                setPosition(e.latlng)   
+                map.flyTo(e.latlng, 13)
+                popup
+                .setLatLng(e.latlng)
+                .setContent("Youre Position is: " + e.latlng.toString())
+                .openOn(map)
+            }
+
+        })
+        return (
+            <Marker position={position}>
+                <Popup>Your location is {position.toString}</Popup>
+            </Marker>
+        )
+    }
+    
     function MyComponent() {
-        const [position, setPosition] = useState(null)
         var popup = L.popup()
         const map = useMapEvents({
             click(e){
                 setPosition(e.latlng)
-                map.flyTo(e.latlng, map.getZoom())
-                popup
-                .setLatLng(e.latlng)
-                .setContent("You Clicked the map at: " + e.latlng.toString())
-                .openOn(map)
+                map.flyTo(e.latlng, 13)
+                
             }
         })
-        return
+        //CRASHES WHEN THIS MARKER IS CLICKED
+        return (
+            <Marker position={position}>
+
+                <Popup>You Clicked at {position.toString}</Popup>
+            </Marker>
+        )
       }
 
     return(
-        <MapContainer center={[42.350876, -71.106918]} zoom={13} scrollWheelZoom={true}>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <MyComponent/>
-        </MapContainer>
+        <div id ="mapandsidebar">
+            <div id="sidebar">
+                <ul>
+                    <li>
+                        <h1>LAT: </h1>
+                    </li>
+                    <li>
+                        <h1>LONG: </h1>
+                    </li>
+                    <li>
+                        <h1>CITY: </h1>
+                    </li>
+
+                </ul>
+
+            </div>
+            <div id = "map">
+                <MapContainer center={[42.350876, -71.106918]} zoom={13} scrollWheelZoom={true}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <LocateUser/>
+                    <MyComponent/>
+                </MapContainer>
+            </div>
+        </div>
     )
 }
 
